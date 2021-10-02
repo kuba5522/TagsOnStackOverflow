258b9cdf47ce77688fb391c98af863e0560fb87e
Jakub Olejnik <49254288+kuba5522@users.noreply.github.com>
2021-10-02 17:42:05 +0200

    Update HomeController.cs

--- TagsOnStackOverflow/Controllers/HomeController.cs
+++ TagsOnStackOverflow/Controllers/HomeController.cs
@@ -20,41 +20,55 @@ namespace TagsOnStackOverflow.Controllers
         {
             _tags = new List<Tag>();
         }
+
         public async Task<ActionResult> Index()
         {
             //10 pages of items
             for (int i = 0; i < 10; i++)
             {
-                HttpClientHandler handler = new HttpClientHandler();
-                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-                HttpClient client = new HttpClient(handler);
-                string apiUrl = ($"https://api.stackexchange.com/2.3/tags?page=" + i+1 + "&pagesize=100&order=desc&sort=popular&site=stackoverflow");
-                //setup http client
-                client.BaseAddress = new Uri(apiUrl);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                //make request
-                string response = await client.GetStringAsync(apiUrl);
+                string response = await HttpHandlerAsync(i + 1);
                 if (response != null)
-                {
-                    //json decode
-                    JObject json = JObject.Parse(response);
-                    for (int j = 0; j < 100; j++)
-                    {
-                        string name = (string)json.SelectToken("items[" + j + "].name");
-                        int count = (int)json.SelectToken("items[" + j + "].count");
-                        _tags.Add(new Tag() {
-                            ID = i * 100 + j + 1,   //1-1000
-                            Name = name,
-                            NumberOfOccurrences = count,
-                            Popularity = string.Empty }
-                         );
-                    }
-                }
+                    JsonDecode(response, i);
             }
             //calculate popularity of tags
             int sum = _tags.Sum(x => x.NumberOfOccurrences);
             _tags.ForEach(x => x.Popularity = string.Format("{0:P3}", (double) x.NumberOfOccurrences / sum));
             return View(_tags);
+        }
+
+        private async Task<string> HttpHandlerAsync(int page)
+        {
+            string apiUrl = ($"https://api.stackexchange.com/2.3/tags?page=" + page + "&pagesize=100&order=desc&sort=popular&site=stackoverflow");
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            HttpClient client = new HttpClient(handler);
+
+            //setup http client
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //make request
+            string response = await client.GetStringAsync(apiUrl);
+
+            return response;
+        }
+
+        private void JsonDecode(string response, int page)
+        {
+            JObject json = JObject.Parse(response);
+            for (int j = 0; j < 100; j++)   //foreach item in json
+            {
+                string name = (string)json.SelectToken("items[" + j + "].name");
+                int count = (int)json.SelectToken("items[" + j + "].count");
+                _tags.Add(new Tag()
+                {
+                    ID = page * 100 + j + 1,   //1-1000
+                    Name = name,
+                    NumberOfOccurrences = count,
+                    Popularity = string.Empty
+                }
+                );
+            }
         }
     }
 }
